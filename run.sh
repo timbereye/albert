8f97713730d5fcc512c6a22332279b7947d200fb
chenxiang <chenxiang@myhexin.com>
2020-05-14 22:07:15 +0800

    ''

--- run.sh
+++ run.sh
@@ -31,7 +31,7 @@ python3 run_extraction.py \
   --train_batch_size=40 \
   --predict_batch_size=40 \
   --learning_rate=5e-5 \
-  --num_train_epochs=16.0 \
+  --num_train_epochs=8.0 \
   --warmup_proportion=.1 \
   --save_checkpoints_steps=10000 \
   --n_best_size=20 \
