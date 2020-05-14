240d147d9cb78ca3a9f16618b042a2bec34b2f32
chenxiang <chenxiang@myhexin.com>
2020-05-14 17:27:11 +0800

    ''

--- squad_utils.py
+++ squad_utils.py
@@ -1551,7 +1551,7 @@ def v2_model_fn_builder(albert_config, init_checkpoint, learning_rate,
 
             train_op = optimization.create_optimizer(
                 total_loss, learning_rate, num_train_steps, num_warmup_steps, use_tpu)
-
+            print("tvars: ", tf.trainable_variables())
             output_spec = contrib_tpu.TPUEstimatorSpec(
                 mode=mode,
                 loss=total_loss,
