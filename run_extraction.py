8f891c29dd65328d49e9679b5f435935ca58a67a
chenxiang <chenxiang@myhexin.com>
2020-05-15 10:30:04 +0800

    1e-4

--- run_extraction.py
+++ run_extraction.py
@@ -276,8 +276,8 @@ def main(_):
     #     input_file=FLAGS.train_file, is_training=True)
     # num_train_steps = int(
     #     len(train_examples) / FLAGS.train_batch_size * FLAGS.num_train_epochs)
-    # if FLAGS.do_train:
-    #     num_warmup_steps = int(num_train_steps * FLAGS.warmup_proportion)
+    if FLAGS.do_train:
+        num_warmup_steps = int(num_train_steps * FLAGS.warmup_proportion)
     #
     #   # Pre-shuffle the input to avoid having to make a very large shuffle
     #   # buffer in in the `input_fn`.
